0c01d6bdaf2fccb02993c853c3ba2f31681ed0fa
ciembor <pub@ciemborowicz.pl>
2015-02-06 15:27:27 +0100

    More aliases in DSL.

--- lib/frogger-logger/dsl.rb
+++ lib/frogger-logger/dsl.rb
@@ -8,6 +8,26 @@ module FroggerLogger
         def frog(message)
           FroggerLogger.log(message)
         end
+
+        def froglog(message)
+          FroggerLogger.log(message)
+        end
+
+        def frogdebug(message)
+          FroggerLogger.debug(message)
+        end
+
+        def froginfo(message)
+          FroggerLogger.info(message)
+        end
+
+        def frogwarn(message)
+          FroggerLogger.warn(message)
+        end
+
+        def frogerr(message)
+          FroggerLogger.err(message)
+        end
       end
     end
   end
